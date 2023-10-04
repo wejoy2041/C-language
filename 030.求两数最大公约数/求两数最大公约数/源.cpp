7656a1e2ee7da73f7d9ed78aa59577353ef21236
wejoy <140638323+wejoy2041@users.noreply.github.com>
2023-10-04 14:05:17 +0800

    Add files via upload

---
+++ 030.求两数最大公约数/求两数最大公约数/源.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int m = 0;
+	int n = 0;
+	int r = 0;
+	scanf("%d%d", &m, &n);
+	while (r=m % n)
+	{
+		//r = m % n;
+		m = n;
+		n = r;
+	}
+	printf("%d\n", n);
+	return 0;
+}
+//Õ·×ªÏà³ý·¨
